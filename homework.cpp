5bdd7515f43f103a25eb561aa378aa7544f1f563
Mistahhh <118472546+Mistahhh@users.noreply.github.com>
2025-01-09 22:44:58 +0000

    d2

--- homework.cpp
+++ homework.cpp
@@ -8,7 +8,7 @@ void displayList(std::vector<T> const &v);
 int main()
 {
     // You can follow along with any code in class here
-    std::vector<list> My_List = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0.01, "Hi"};
+    std::vector<int> My_List = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     ;displayList(My_List, 0)
     return 0;
